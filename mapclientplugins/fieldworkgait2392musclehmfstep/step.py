454f5f134de10db98649779efa2b281256a45624
Hugh Sorby <h.sorby@auckland.ac.nz>
2021-06-04 13:17:29 +1200

    Update step.py

--- mapclientplugins/fieldworkgait2392musclehmfstep/step.py
+++ mapclientplugins/fieldworkgait2392musclehmfstep/step.py
@@ -138,7 +138,7 @@ class FieldworkGait2392MuscleHMFStep(WorkflowStepMountPoint):
         '''
         self._config.update(json.loads(string))
 
-        d = ConfigureDialog(self._main_window)
+        d = ConfigureDialog()
         d.identifierOccursCount = self._identifierOccursCount
         d.setConfig(self._config)
         self._configured = d.validate()
