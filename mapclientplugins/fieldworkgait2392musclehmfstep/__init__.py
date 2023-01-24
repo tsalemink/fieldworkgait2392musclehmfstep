81fb61f48bdfe525478eef48930deea6f936414f
TimmyNic <tim.nicolas@outlook.com>
2023-01-24 15:52:48 +1300

    Set version to 0.2.0

--- mapclientplugins/fieldworkgait2392musclehmfstep/__init__.py
+++ mapclientplugins/fieldworkgait2392musclehmfstep/__init__.py
@@ -2,7 +2,7 @@
 MAP Client Plugin
 '''
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __author__ = 'Ju Zhang'
 __stepname__ = 'Fieldwork Gait2392 Muscle HMF'
 __location__ = 'https://github.com/mapclient-plugins/fieldworkgait2392musclehmfstep/archive/v0.1.0.zip'
